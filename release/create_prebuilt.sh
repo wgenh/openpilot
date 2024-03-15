16f145485e9f215ae76db2bd6c53fc160b170d28
Comma Device <device@comma.ai>
2024-03-15 19:57:07 +0000

    cd target dir

--- release/create_prebuilt.sh
+++ release/create_prebuilt.sh
@@ -8,6 +8,8 @@ SOURCE_DIR=$2
 rm -f panda/board/obj/panda.bin.signed
 rm -f panda/board/obj/panda_h7.bin.signed
 
+cd $TARGET_DIR
+
 VERSION=$(cat common/version.h | awk -F[\"-]  '{print $2}')
 echo "#define COMMA_VERSION \"$VERSION-release\"" > common/version.h
 
